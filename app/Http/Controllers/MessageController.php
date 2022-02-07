657cba48dcf838e620629c8e9afa8e1372f8d865
Gako3113 <shimizu.koga.31@gmail.com>
2022-02-07 23:56:58 +0900

    fix pusher3

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+
 class MessageController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
-        $id = Auth::id();
-        $users = User::find(1)->get();
-        dd($users);
+        $users = User::select('id','name')->get();
+        // $user = Auth::user();
+        // $users = User::find(1)->get();
+        // dd($users);
         // ログイン者以外のユーザを取得する
-        $users = User::where('id' , $user->id)->get();
+        // $users = User::where('id' , $user->id)->get();
         // チャットユーザ選択画面を表示
         return view('message.chat_user_select', compact('users'));
     }

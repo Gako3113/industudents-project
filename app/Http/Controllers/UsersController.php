93dcb61828aa91df90596c322ef02f4d321d13f0
Gako3113 <shimizu.koga.31@gmail.com>
2022-02-16 00:32:55 +0900

    follows

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\UserFollowed;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,37 @@ class UsersController extends Controller
         $user = User::all()->first();
         $users = User::where('id', '!=', $user->id)->get();
         return view('users.index', compact('users'));
+    }
+
+    public function follow(User $user)
+    {
+        $follower = auth()->user();
+        if ($follower->id == $user->id) {
+            return back()->withError("You can't follow yourself");
+        }
+        if(!$follower->isFollowing($user->id)) {
+            $follower->follow($user->id);
+
+            // sending a notification
+            $user->notify(new UserFollowed($follower));
+
+            return back()->withSuccess("You are now friends with {$user->name}");
+        }
+        return back()->withError("You are already following {$user->name}");
+    }
+
+    public function unfollow(User $user)
+    {
+        $follower = auth()->user();
+        if($follower->isFollowing($user->id)) {
+            $follower->unfollow($user->id);
+            return back()->withSuccess("You are no longer friends with {$user->name}");
+        }
+        return back()->withError("You are not following {$user->name}");
+    }
+
+    public function notifications()
+    {
+        return auth()->user()->unreadNotifications()->limit(5)->get()->toArray();
     }
 }
